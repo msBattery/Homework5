ae3f17e291dc5d09da604692e680a9807eb0f344
msBattery <91663413+msBattery@users.noreply.github.com>
2026-04-24 10:22:05 +0300

    Homework 5_rev_2

--- src/Main.java
+++ src/Main.java
@@ -12,14 +12,14 @@ public class Main {
             System.out.println("Ваша установленная OS не поддерживается");
         }
         System.out.println("Вот решение задания 2:");
-        int releaseYear = 2014;
-        if (clientOS == 0 && releaseYear < 2015) {
+        int clientDeviceYear = 2015;
+        if (clientOS == 0 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (clientOS == 1 && releaseYear < 2015) {
+        } else if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else if (clientOS == 1) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOS == 0) {
             System.out.println("Установите версию приложения для Android по ссылке");
         } else {
             System.out.println("Ваша установленная OS не поддерживается");
